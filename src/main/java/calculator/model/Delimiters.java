7d7cdb9b0024b408586fcf768993c9e3c8d9b4e9
KimRPG <120882082+KimRPG@users.noreply.github.com>
2025-10-19 17:56:06 +0900

    fix(delimiter): 커스텀 구분자가 있을 시 패턴을 제외한 문자열 반환

--- src/main/java/calculator/model/Delimiters.java
+++ src/main/java/calculator/model/Delimiters.java
@@ -7,32 +7,50 @@ import java.util.regex.Pattern;
  * 구분자 클래스
  */
 public class Delimiters {
+    private String numberText;
+    private final String delimiterRegex;
 
     private static final Pattern CUSTOM_SEPARATOR = Pattern.compile("//(.*)\\\\n(.*)");
-    private final StringBuilder sb = new StringBuilder();
 
     /**
-     * 구분자를 반환하는 클래스
+     * 생성자 구분자를 제거한 텍스트와 구분자를 설정한다
      *
      * @param text 사용자 입력 텍스트
-     * @return 구분자 반환
      */
-    public String getDelimiters(String text) {
-        Matcher customDelimiterMatcher = customDelimiterMatcher(text);
+    public Delimiters(String text) {
         //Default 구분자
         String delimiters = ",|:";
-        sb.append("[").append(delimiters);
 
-        // 커스텀 구분자가 존재할 시에는 커스텀 구분자를 추가하여 구분자를 반환한다.
+        StringBuilder sb = new StringBuilder();
+        Matcher customDelimiterMatcher = customDelimiterMatcher(text);
+
+        sb.append("[").append(delimiters);
+        this.numberText = text;
+        // 커스텀 구분자가 존재할 경우
         if (customDelimiterMatcher.matches()) {
             sb.append("|").append(customDelimiterMatcher.group(1));
+            this.numberText = customDelimiterMatcher.group(2);
         }
         sb.append("]");
 
-        return sb.toString();
+        delimiterRegex = sb.toString();
     }
 
     private Matcher customDelimiterMatcher(String text) {
         return CUSTOM_SEPARATOR.matcher(text);
+    }
+
+    /**
+     * @return 커스텀 구분자를 제거한 텍스트
+     */
+    public String getNumberText() {
+        return numberText;
+    }
+
+    /**
+     * @return 구분자 반환
+     */
+    public String getDelimiterRegex() {
+        return delimiterRegex;
     }
 }
