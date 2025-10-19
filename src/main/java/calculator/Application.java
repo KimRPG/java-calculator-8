dadba6598f7fb6f39c4631e908c13a190866db92
KimRPG <120882082+KimRPG@users.noreply.github.com>
2025-10-19 20:20:03 +0900

    feat(app): MVC 컴포넌트 의존성 주입 및 애플리케이션 실행

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,24 @@
 package calculator;
 
+import calculator.controller.CalculatorController;
+import calculator.model.IntegerParser;
+import calculator.model.IntegerParserImpl;
+import calculator.model.SumCalculator;
+import calculator.model.SumCalculatorImpl;
+import calculator.view.InputView;
+import calculator.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        InputView inputView = new InputView();
+        IntegerParser integerParser = new IntegerParserImpl();
+        SumCalculator sumCalculator = new SumCalculatorImpl();
+        OutputView outputView = new OutputView();
+        CalculatorController controller = new CalculatorController(inputView,
+                integerParser, sumCalculator, outputView);
+
+        controller.run();
+
     }
 }
