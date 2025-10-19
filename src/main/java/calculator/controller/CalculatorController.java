6d042bf3ad001eb1f8ac5efb4867a3947f264fe3
KimRPG <120882082+KimRPG@users.noreply.github.com>
2025-10-19 19:34:29 +0900

    feat(controller): CalculatorController 추가

--- src/main/java/calculator/controller/CalculatorController.java
+++ src/main/java/calculator/controller/CalculatorController.java
@@ -1,15 +1,39 @@
 package calculator.controller;
 
+import calculator.model.IntegerParser;
+import calculator.model.SumCalculator;
 import calculator.view.InputView;
+import calculator.view.OutputView;
+import java.util.List;
 
+/**
+ * 계산기 Controller
+ */
 public class CalculatorController {
     private final InputView inputView;
+    private final IntegerParser integerParser;
+    private final SumCalculator sumCalculator;
+    private final OutputView outputView;
 
-    public CalculatorController(InputView inputView) {
+
+    public CalculatorController(InputView inputView,
+                                IntegerParser integerParser,
+                                SumCalculator sumCalculator,
+                                OutputView outputView) {
         this.inputView = inputView;
+        this.integerParser = integerParser;
+        this.sumCalculator = sumCalculator;
+        this.outputView = outputView;
+
     }
 
+    /**
+     * Application을 실행한다.
+     */
     public void run() {
-        String a = inputView.inputView();
+        String text = inputView.inputView();
+        List<Integer> integerList = integerParser.ParserToIntList(text);
+        int result = sumCalculator.sum(integerList);
+        outputView.output(result);
     }
 }
